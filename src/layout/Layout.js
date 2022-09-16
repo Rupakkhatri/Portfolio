696f07b27f118457c3d44757862cd10bcc4f8d25
rupakkhatri <rkhatri@mail.sfsu.edu>
2022-09-16 06:40:29 -0700

    Test

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
-
-export const Layout = ({children}) => {
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import { Container } from "./LayoutStyles";
+//TEST
+export const Layout = ({ children }) => {
   return (
     <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
